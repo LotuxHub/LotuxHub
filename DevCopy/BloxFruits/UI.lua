65ef019faed6cc76a9150ffce128a45b6c362473
biellucas12102010-source <biellucas12102010@gmail.com>
2026-05-15 22:10:57 -0300

    Update UI.lua

--- DevCopy/BloxFruits/UI.lua
+++ DevCopy/BloxFruits/UI.lua
@@ -549,7 +549,7 @@ end)
 -- =====================================================
 -- NOTIFICACOES
 -- =====================================================
-local IMG     = "rbxassetid://135350717440671"
+local IMG     = "rbxassetid://93533571632029"
 local uiReady = false
 local function Notify(cfg)
     if not uiReady then return end
